f6e0c15a8e0763308cf425a35f165708506ae58e
An Huynh <110524240+anhuynh1234@users.noreply.github.com>
2023-06-16 16:15:12 -0600

    Added DOM Javascript Exercises

---
+++ OdinProject/Foundations/practices/index.js
@@ -0,0 +1,29 @@
+
+const content = document.querySelector('body #container');
+const p = document.createElement('p');
+p.textContent = "Hey! I'm red!";
+p.style.color = 'red';
+p.classList.add('para');
+content.appendChild(p);
+
+const h3 = document.createElement('h3');
+h3.textContent = 'Hey! I\'m a blue h3!!';
+h3.style.color = 'blue';
+h3.style['background-color'] = "grey";
+h3.setAttribute('id', 'heading3');
+content.appendChild(h3);
+
+const div = document.createElement('div');
+const newP = document.createElement('p');
+const newh1 = document.createElement('h1');
+
+newP.textContent = "ME TOO!!";
+newh1.textContent = "I'm a div!!";
+
+div.style['background-color'] = "#FFC0CB";
+div.style.cssText = 'border-color: black; border-style: solid';
+
+div.appendChild(newh1);
+div.appendChild(newP);
+
+content.appendChild(div);
