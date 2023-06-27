f8e20a8e6ee9b79f692bfb6f5db428a13bd273d2
An Huynh <110524240+anhuynh1234@users.noreply.github.com>
2023-06-27 11:10:48 -0600

    Added Event Listener for numb pad

--- OdinProject/Foundations/calculator/script.js
+++ OdinProject/Foundations/calculator/script.js
@@ -1,3 +1,14 @@
+let errorMath = false
+let line = ""
 
 const body = document.querySelector('body')
 
+const numbKeys = document.querySelectorAll('.numbers')
+const display = document.querySelector(".display .text")
+
+numbKeys.forEach((number) => {
+    number.addEventListener('click', () => {
+        line += number.innerText;
+        display.innerText = line;
+    })
+})
